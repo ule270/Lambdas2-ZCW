c696a27e20fe413e6432e5997c98a124e911676d
ule270 <u.le270@gmail.com>
2023-04-20 16:19:33 -0400

    Person class made

---
+++ src/Main/Person.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+
+public class Person {
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public int getAge() {
+        // ...
+    }
+
+    public void printPerson() {
+        // ...
+    }
+}
